85730e10b79b3d37118302b3c84075106599d84e
jovany velez <jovany.velez@udea.edu.co>
2023-06-14 06:35:10 -0500

    uno

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -47,15 +47,8 @@ const login: Action = async ({ cookies, request }) => {
 		cookies.set('session', authenticatedUser.userAuthToken, {
 			// send cookie for every page
 			path: '/',
-			// server side only cookie so you can't use `document.cookie`
-			httpOnly: true,
-			// only requests from same site can send cookies
-			// https://developer.mozilla.org/en-US/docs/Glossary/CSRF
-			sameSite: 'strict',
 			// only sent over HTTPS in production
-			secure: false,
-			// set cookie to expire after a month
-			maxAge: 60 * 60 * 24 * 30
+			secure: false
 		});
 	} catch (error) {
 		// database connection error
