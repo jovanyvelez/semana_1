7f201d160392e154b77677e601ccd0a93d669dfe
jovany velez <jovany.velez@udea.edu.co>
2023-06-26 21:39:47 -0500

    actualizar port

--- src/lib/server/nodemailer.js
+++ src/lib/server/nodemailer.js
@@ -4,7 +4,7 @@ import { env } from '$env/dynamic/private';
 // create reusable transporter object using the default SMTP transport
 export let transporter = nodemailer.createTransport({
 	host: env.NODE_MAILER_HOST,
-	port: env.NODE_MAILER_PORT,
+	port: parseInt(env.NODE_MAILER_PORT),
 	secure: true, // true si uso puerto 465, falso para otros puertos
 	auth: {
 		user: env.NODE_MAILER_USER, 
