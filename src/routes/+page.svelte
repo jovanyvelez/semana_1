1d1aef3351d15270cd65b3898c30d7dc19fa3e07
jovany velez <jovany.velez@udea.edu.co>
2023-06-26 19:59:26 -0500

    testing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,6 +10,7 @@
 			{#each elResultado as category (category.rootid)}
 				<h1 class="my-10 text-center text-blue-600">{category.name}</h1>
 				{#each category.products as product (product.id)}
+					<pre>{images}</pre>
 					<div class="card glass w-48 sm:w-64 bg-base-100 shadow-xl mx-2 mt-2">
 						<figure><img src={images==='true' ? product.image[0].secureUrl:product.image[0].publicId} alt="article" class="" /></figure>
 						<div class="card-body">
