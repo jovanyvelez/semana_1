855096aea2c829483a27021f52f1f1c4ba02affa
jovany velez <jovany.velez@udea.edu.co>
2023-07-20 14:48:28 -0500

    Carrito modificado

--- src/lib/components/ButonQuantity.svelte
+++ src/lib/components/ButonQuantity.svelte
@@ -71,23 +71,31 @@
 		Comprar
 	</button>
 {:else}
-	<div>
-		<button
-			on:click={() => outOfCart(mProduct)}
-			class='btn btn-sm btn-outline btn-primary'
-		>
-			-
-		</button>
-		<span class="mx-2" use:jovanyActions={quantity}></span>
-		<button
-			disabled={disabled}
-			on:click={() => addToCart(mProduct)}
-			class="{disabled ? 'btn btn-sm btn-outline':'btn btn-sm btn-outline btn-primary'}"
-		>
-			+
-		</button>
-		{#if cost > 0 && mostrarCosto}
-			<span use:jovanyActions={`$${cost}`} />
-		{/if}
-	</div>
+	
+		<div class="flex items-center justify-evenly">
+			<button
+				on:click={() => outOfCart(mProduct)}
+				class='btn btn-link'
+			>
+				{#if quantity===1}
+				<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 72 72"><g transform="translate(72 0) scale(-1 1)"><path fill="#D0CFCE" d="M15.076 12.068v5.951h35.745l.644-5.951zm6.833 42.621l.5 5.282h21.518l.592-5.282z"/><path fill="#9B9B9A" d="M42.096 60.178h7.323l.515-5.731h-7.238zm14.433-47.89h-7.59l-.628 5.613h8.218z"/><g fill="none" stroke="#000" stroke-miterlimit="10"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m21.38 50.393l-3.607-28.381m36.49-.029l-3.757 28.395m-9.93-28.289l8.173 10.02M30.417 22.13l17.152 20.642M22.352 24.253l21.864 26.172M23.902 36.996L35.928 50.63m-10.611-1.234l1.363 1.357m-3.452-19.519l8.585-9.131M24.61 41.882L42.853 22.13m-15.968 28.5l22.497-23.915M36.574 50.63l11.543-11.946m-3.901 12.069l2.127-1.793M14.905 12.028H56.95v5.946H14.905z"/><path d="M42.853 54.404h7.271"/><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m50.124 54.404l-.708 5.562H22.43l-.681-5.562h28.375"/></g></g></svg>
+				{:else}
+				<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 50 50"><g transform="translate(50 0) scale(-1 1)"><path fill="currentColor" d="M25 42c-9.4 0-17-7.6-17-17S15.6 8 25 8s17 7.6 17 17s-7.6 17-17 17zm0-32c-8.3 0-15 6.7-15 15s6.7 15 15 15s15-6.7 15-15s-6.7-15-15-15z"/><path fill="currentColor" d="M16 24h18v2H16z"/></g></svg>
+				{/if}
+			</button>
+			<span class=" text-lg font-semibold" use:jovanyActions={quantity}></span>
+			{#if !disabled}
+				<button
+					on:click={() => addToCart(mProduct)}
+					class="btn btn-link"
+				>
+					<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 50 50"><g transform="translate(50 0) scale(-1 1)"><path fill="currentColor" d="M25 42c-9.4 0-17-7.6-17-17S15.6 8 25 8s17 7.6 17 17s-7.6 17-17 17zm0-32c-8.3 0-15 6.7-15 15s6.7 15 15 15s15-6.7 15-15s-6.7-15-15-15z"/><path fill="currentColor" d="M16 24h18v2H16z"/><path fill="currentColor" d="M24 16h2v18h-2z"/></g></svg>
+				</button>
+			{/if}
+		
+			{#if cost > 0 && mostrarCosto}
+				<span use:jovanyActions={`$${cost}`} />
+			{/if}
+		
+</div>
 {/if}
--- src/routes/carrito/+page.svelte
+++ src/routes/carrito/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import ButonQuantity from '$lib/components/ButonQuantity.svelte';
-	import { jovanyActions } from '$lib/jovanyActions';
 	import { cart } from '$lib/stores/stores';
 	import { goto } from '$app/navigation';
 
@@ -8,8 +7,14 @@
 	export let data;
 
 	let empty: boolean = false;
-
+	let yes = false;
 	let resum: { total: number; items: number };
+
+	const outOfCart = (product:string) => {
+		const index = $cart.findIndex((x) => x.id === product);
+		$cart.splice(index, 1);
+		$cart = $cart;
+	};
 
 	$cart.length > 0 ? (empty = false) : (empty = true);
 
@@ -26,53 +31,69 @@
 			items: $cart.reduce((a, c) => a + c.qtyBuy, 0)
 		});
 </script>
+
 <h1 class="my-16 text-center">TU CARRITO</h1>
 {#if !empty}
-	<div class="flex flex-col md:flex-row w-10/12 mx-auto">
-		<div class="">
-			{#each $cart as product (product.id)}
-				<div
-					class="flex flex-col border border-primary my-2 rounded-lg w-full"
-				>
-					<small class="text-left font-bold mx-4 mt-2">{product.name}</small>
-
-					<div class="flex items-center">
-						<div class="flex items-center mr-3">
-							<img src={product.image[0].secureUrl} alt="product" class="w-32 mb-2 mx-3" />
-							<ButonQuantity {product} />
-						</div>
-
-						<div class="relative mx-5">
-							<div class="z-100">
-								<p>{(product.price[0].price1).toLocaleString('es-ES', { style: 'currency', currency: 'COP' })}</p>
-							</div>
-							<div
-								class="absolute bottom-0 left-1/2 transform -translate-x-1/2 -rotate-90 translate-y-1/3 z-50"
-							>
-								<p class="text-sm font-bold text-blue-200 text-opacity-95">
-									{data.name}
-								</p>
-							</div>
-						</div>
-					</div>
-				</div>
-			{/each}
+<div class="grid grid-cols-1 sm:grid-cols-4 mx-5 sm:mx-10 lg:mx-36 my-5">
+	<div class="flex justify-center items-center"></div>
+	<div class="hidden sm:block text-center font-semibold">Precio</div>
+	<div class="hidden sm:block text-center font-semibold">Cantidad</div>
+	<div class="hidden sm:block text-center font-semibold">Total</div>
+	{#each $cart as product (product.id)}
+		<div class="divider sm:col-span-4 mx-10"></div>
+		<div class="flex flex-col items-center justify-center">
+			<img src={product.image[0].secureUrl} alt="product" class="w-20 mb-2 mx-3" />
+			<div class="flex">
+				<button on:click={() => outOfCart(product.id)}>
+					<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 72 72"><g transform="translate(72 0) scale(-1 1)"><path fill="#D0CFCE" d="M15.076 12.068v5.951h35.745l.644-5.951zm6.833 42.621l.5 5.282h21.518l.592-5.282z"/><path fill="#9B9B9A" d="M42.096 60.178h7.323l.515-5.731h-7.238zm14.433-47.89h-7.59l-.628 5.613h8.218z"/><g fill="none" stroke="#000" stroke-miterlimit="10"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m21.38 50.393l-3.607-28.381m36.49-.029l-3.757 28.395m-9.93-28.289l8.173 10.02M30.417 22.13l17.152 20.642M22.352 24.253l21.864 26.172M23.902 36.996L35.928 50.63m-10.611-1.234l1.363 1.357m-3.452-19.519l8.585-9.131M24.61 41.882L42.853 22.13m-15.968 28.5l22.497-23.915M36.574 50.63l11.543-11.946m-3.901 12.069l2.127-1.793M14.905 12.028H56.95v5.946H14.905z"/><path d="M42.853 54.404h7.271"/><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m50.124 54.404l-.708 5.562H22.43l-.681-5.562h28.375"/></g></g></svg>
+				</button>
+				<p class="font-bold mx-4 mt-2 text-center">{product.name}</p>
+			</div>
 		</div>
-		<div class="flex flex-col mx-auto justify-center ">
-			<!--span use:jovanyActions={`Items(${resum.items}) `} /-->
-			{#key resum}
-				<h1 class="text-sm md:text-3xl"><span class="text-blue-500">Total de equipos:</span> {resum.items}</h1>
-				<h1 class="text-sm md:text-3xl"><span class="text-blue-500">Valor:</span> {(resum.total).toLocaleString('es-ES', { style: 'currency', currency: 'COP' })}</h1>	
-			{/key}
+		<div class="flex justify-center items-center">
+			${(product.price[0].price1).toLocaleString()}
+		</div>
+		<div class="flex flex-col justify-center items-center">
+			<ButonQuantity {product} />
+		</div>
+		<div class="flex justify-center items-center">
+			${(product.price[0].price1*product.qtyBuy).toLocaleString()}
+		</div>
+		<div class="divider  mx-10  sm:col-span-4"></div>
+	{/each}
+</div>
+
+<div class="flex justify-end mx-5 sm:mx-16 lg:mx-36 my-0">
+	<div class="flex flex-col mr-10 text-end">
+		{#key resum}
+			<h1 class="text-slate-500 text-sm lg:text-lg">SUBTOTAL: ${(resum.total).toLocaleString()}</h1>	
+		{/key}
+		<h1 class="text-slate-400 text-sm lg:text-lg">
+			Iva incluido. Envío y descuentos agregados en el checkout
+		</h1>
+		<div class="flex justify-end">
+			<label class="label cursor-pointer">
+				<input type="checkbox" bind:checked={yes}  class="checkbox mr-1" />
+				<span class=" font-semibold text-slate-400 text-sm lg:text-lg">Acepto los terminos y condiciones</span> 
+			</label>
 		</div>
 	</div>
+</div>
 
-	<div class="flex flex-wrap justify-center">
-		<button on:click={() => goto('/tienda/address')} type="submit" class="btn btn-primary btn-wide"
-			>Hacer compra</button
-		>
-	</div>
+<div class="flex flex-wrap justify-around my-5">
+	<a href='/tienda/' type="submit" class="btn btn-outline btn-primary btn-wide"
+		>SEGUIR COMPRANDO</a
+	>
+	<button on:click={() => goto('/tienda/address')} type="submit" class="btn btn-primary btn-wide {!yes? 'btn-disabled ':''}"
+		>CHECKOUT</button
+	>
+</div>
+
 {:else}
 	<span class="mr-2">No has hecho compras</span>
 	<a href="/" class="text-blue-500">Regresar a la tienda</a>
 {/if}
+
+
+
+
