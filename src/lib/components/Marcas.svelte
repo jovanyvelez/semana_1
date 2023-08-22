1f91c84fb9ecd83f34f248e28257a7578a371f93
jovany velez <jovany.velez@udea.edu.co>
2023-08-22 12:11:09 -0500

    logos marcas

--- src/lib/components/Marcas.svelte
+++ src/lib/components/Marcas.svelte
@@ -1,6 +1,6 @@
-<div class="flex flex-wrap justify-around m-10">
-     <a href="/tienda/d1eefb93-05f4-45f0-b72b-47ad6a256c0b"><img class="w-32 h-24" src="parker.jpg" alt="riello"></a>
-    <img class="w-32 h-24" src="riello.png" alt="riello">
-    <img class="w-32 h-24" src="pietro.png" alt="riello">
-    <img class="w-32 h-24" src="alfaLaval.jpg" alt="riello">
+<div class="flex flex-col sm:flex-row sm:flex-auto sm:flex-wrap justify-around m-10 items-center">
+     <a href="/tienda/d1eefb93-05f4-45f0-b72b-47ad6a256c0b"><img src="parker.jpg" alt="riello"></a>
+    <img class="my-5" src="riello.png" alt="riello">
+    <img class="my-5" src="pietro.png" alt="riello">
+    <img class="my-5" src="alfaLaval.jpg" alt="riello">
 </div>
