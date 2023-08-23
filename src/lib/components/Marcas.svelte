18e9b902520a830a5d9a0efdaee5ea6c992ca80f
jovany velez <jovany.velez@udea.edu.co>
2023-08-22 23:14:57 -0500

    marcas menu

--- src/lib/components/Marcas.svelte
+++ src/lib/components/Marcas.svelte
@@ -1,6 +1,6 @@
 <div class="flex flex-col sm:flex-row sm:flex-auto sm:flex-wrap justify-around m-10 items-center">
-     <a href="/tienda/d1eefb93-05f4-45f0-b72b-47ad6a256c0b"><img src="parker.jpg" alt="riello"></a>
-    <img class="my-5" src="riello.png" alt="riello">
-    <img class="my-5" src="pietro.png" alt="riello">
-    <img class="my-5" src="alfaLaval.jpg" alt="riello">
+	<a href="/tienda/d1eefb93-05f4-45f0-b72b-47ad6a256c0b"><img class="my-5" src="parker.jpg" alt="riello" /></a>
+	<a href="/tienda/b179eb91-0cd4-43ef-a16e-b73e67ddcb86"><img class="my-5" src="riello.png" alt="riello" /> </a>
+	<a href="/tienda/fc308c96-7e8e-4381-8c4d-8d5d71f5d484"><img class="my-5" src="pietro.png" alt="riello" /> </a>
+	<a href="/tienda/e822fef6-6f74-4de7-9c42-9ce9b03ce6b8"><img class="my-5" src="alfaLaval.jpg" alt="riello" /></a>
 </div>
